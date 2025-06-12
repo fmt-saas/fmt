01314780e4faede5935b83f43a710fe517411e6f
Cédric Françoys <cedricfrancoys@gmail.com>
2025-06-12 16:49:55 +0200

    followup

---
+++ fmt/tests/accounting-miscoperation.php
@@ -0,0 +1,66 @@
+<?php
+/*
+    This file is part of FMT SaaS Software <https://github.com/fmt-saas/fmt>
+    Some Rights Reserved, FMT SRL, 2025-2026
+    Original author(s): Yesbabylon SA
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+
+use finance\accounting\MiscOperation;
+use finance\accounting\MiscOperationLine;
+use finance\bank\CondominiumBankAccount;
+
+$providers = eQual::inject(['context', 'orm', 'auth', 'access']);
+
+$tests = [
+
+    '1101' => [
+            'description'       => "Accounting entry with non-balanced lines.",
+            'help'              => "Create an accounting entry, with 2 balanced lines. Entry balance test is expected to return true.",
+            'return'            => ['boolean'],
+            'arrange'           => function() use($providers) {
+                    $miscOperation = MiscOperation::create([
+                            'condo_id'          => 1,
+                            'description'       => 'reprise de compte epargne',
+                            'posting_date'      => time(),
+                            'fiscal_year_id'    => 1,
+                            'fiscal_period_id'  => 1,
+                            'journal_id'        => 5,
+                            'operation_type'    => 'misc'
+                        ])
+                        ->first();
+
+                    MiscOperationLine::create([
+                            'condo_id'          => 1,
+                            'misc_operation_id' => $miscOperation['id'],
+                            'account_id'        => 676,
+                            'journal_id'        => 11,
+                            'credit'            => 5000
+                        ]);
+
+                    MiscOperationLine::create([
+                            'condo_id'          => 1,
+                            'misc_operation_id' => $miscOperation['id'],
+                            'account_id'        => 468,
+                            'journal_id'        => 11,
+                            'debit'             => 5000
+                        ]);
+
+                    return $miscOperation;
+                },
+            'act'               => function($miscOperation) use($providers) {
+                    MiscOperation::id($miscOperation['id'])
+                        ->transition('publish')
+                        ->transition('post');
+                },
+            'assert'            => function() use($providers) {
+                    $bankAccount = CondominiumBankAccount::search(['accounting_account_id', '=', 468])
+                        ->read(['available_balance'])
+                        ->first();
+
+                    return $bankAccount['available_balance'] == 5000;
+                },
+            'rollback'          => function() use($providers) {
+                }
+        ]
+];
