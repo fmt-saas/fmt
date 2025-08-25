22b239b8df3f938426596e914633a73c6f153217
Cédric Françoys <cedricfrancoys@gmail.com>
2025-08-25 17:02:01 +0200

    added app

---
+++ fmt/apps/app.php
@@ -0,0 +1,14 @@
+<?php
+/*
+    Developed by Yesbabylon – https://yesbabylon.com
+    (c) 2025–2026 Yesbabylon SA
+    Licensed under the GNU AGPL v3 License – https://www.gnu.org/licenses/agpl-3.0.html
+*/
+
+list($params, $providers) = eQual::announce([
+    'description'   => 'Redirect to `/app` public folder.',
+    'params'        => [],
+    'response'      => [
+        'location'      => '/app/'
+    ]
+]);
