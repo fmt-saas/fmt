5aabe194dfb367234b10de6fd5002f72d745f1d0
Cédric Françoys <cedricfrancoys@gmail.com>
2025-10-01 09:54:40 +0200

    updated

--- finance/classes/accounting/AccountingEntry.class.php
+++ finance/classes/accounting/AccountingEntry.class.php
@@ -393,8 +393,14 @@ class AccountingEntry extends Model {
                     ]);
             }
 
+            $entry_date = $accountingEntry['entry_date'];
+
             // make sure entries are chronological
             // search for all entries in the period for the concerned journal and, if more recent, take the date of the most recent one
+
+            /*
+            // #memo - this has been disabled because the encoding of the purchase invoices is non chronological
+            // - we must maintain the sequence, but cannot force dates sequence without losing information
             $domain = [
                     ['id', '<>', $id],
                     ['fiscal_period_id', '=', $accountingEntry['fiscal_period_id']['id']]
@@ -408,10 +414,11 @@ class AccountingEntry extends Model {
             }
 
             $mostRecentEntry = self::search($domain, ['sort' => ['entry_date' => 'desc'], 'limit' => 1])->read(['entry_date'])->first();
-            $entry_date = $accountingEntry['entry_date'];
+
             if($mostRecentEntry && $mostRecentEntry['entry_date'] > $accountingEntry['entry_date']) {
                 $entry_date = $mostRecentEntry['entry_date'];
             }
+            */
 
             self::id($id)
                 ->update([
