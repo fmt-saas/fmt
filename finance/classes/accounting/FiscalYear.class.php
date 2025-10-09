f8010c610a957d41d6eba9aa426e288ef545e745
Cédric Françoys <cedricfrancoys@gmail.com>
2025-10-09 11:03:31 +0200

    updated

--- finance/classes/accounting/FiscalYear.class.php
+++ finance/classes/accounting/FiscalYear.class.php
@@ -751,6 +751,7 @@ class FiscalYear extends Model {
                 $accountingEntry = AccountingEntry::create([
                         'condo_id'          => $fiscalYear['condo_id'],
                         'journal_id'        => $carryForwardJournal['id'],
+                        'description'       => "Ecritures de report",
                         'is_temp'           => false,
                         'is_carry_forward'  => true,
                         'fiscal_year_id'    => $nextFiscalYear['id'],
@@ -842,7 +843,7 @@ class FiscalYear extends Model {
                 $accountingEntry = AccountingEntry::create([
                         'condo_id'          => $fiscalYear['condo_id'],
                         'journal_id'        => $carryForwardJournal['id'],
-                        'description'       => "Ecritures de report",
+                        'description'       => "Ecritures de report (temp)",
                         'is_temp'           => true,
                         'is_carry_forward'  => true,
                         'fiscal_year_id'    => $nextFiscalYear['id'],
