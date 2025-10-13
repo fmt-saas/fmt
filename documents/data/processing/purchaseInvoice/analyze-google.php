61adbc5dd51202ea8004f9749c9a33aa96c14735
Cédric Françoys <cedricfrancoys@gmail.com>
2025-10-13 12:05:57 +0200

    updated

--- documents/data/processing/purchaseInvoice/analyze-google.php
+++ documents/data/processing/purchaseInvoice/analyze-google.php
@@ -77,7 +77,7 @@ $response = $request->send();
 $status = $response->getStatusCode();
 
 if($status != 200) {
-    trigger_error("APP::Document AI request failed with code $status", EQ_REPORT_ERROR);
+    trigger_error("APP::Document AI request failed with code $status, body: " . $response->body(), EQ_REPORT_ERROR);
     throw new Exception('invalid_analysis_response', EQ_ERROR_UNKNOWN);
 }
 
