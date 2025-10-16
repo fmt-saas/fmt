eaa7936f670fc63eabd3eeee787de4e3785ef58f
Cédric Françoys <cedricfrancoys@gmail.com>
2025-10-16 13:27:49 +0200

    updated

---
+++ infra/classes/server/Instance.class.php
@@ -0,0 +1,58 @@
+<?php
+/*
+    This file is part of Symbiose Community Edition <https://github.com/yesbabylon/symbiose>
+    Some Rights Reserved, Yesbabylon SRL, 2020-2024
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+
+namespace infra\server;
+
+use equal\orm\Model;
+
+class Instance extends Model {
+
+    public static function getDescription() {
+        return 'Instance manages service or product instances, detailing type, version, URL, access information, and running software.';
+    }
+
+    public static function getColumns() {
+
+        return [
+
+            'name'    => [
+                'type'              => 'string',
+                'unique'            => true,
+                'required'          => true,
+                'description'       => 'Unique identifier of the instance.'
+            ],
+
+            'description' => [
+                'type'              => 'string',
+                'usage'             => 'text/plain',
+                'description'       => 'Short description of the instance.'
+            ],
+
+            'instance_type' => [
+                'type'              => 'string',
+                'selection'         => ['master', 'agency'],
+                'description'       => 'Type of instance.',
+                'default'           => 'agency'
+            ],
+
+            'url' => [
+                'type'              => 'string',
+                'usage'             => 'uri/url',
+                'description'       => 'Front-end home URL.'
+            ],
+
+            'server_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'inventory\server\Server',
+                'description'       => 'Server (host) on which the instance runs.',
+                'ondelete'          => 'cascade',
+                'required'          => true
+            ],
+
+        ];
+    }
+}
---
+++ infra/classes/server/Server.class.php
@@ -0,0 +1,68 @@
+<?php
+/*
+    This file is part of Symbiose Community Edition <https://github.com/yesbabylon/symbiose>
+    Some Rights Reserved, Yesbabylon SRL, 2020-2024
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+
+namespace infra\server;
+
+use equal\orm\Model;
+
+class Server extends Model {
+
+
+    public static function getDescription() {
+        return 'A Server hosts one or more instances.';
+    }
+
+    public static function getColumns() {
+
+        return [
+
+            'name' => [
+                'type'              => 'string',
+                'description'       => 'Internal identification ex. trg.be-master.',
+                'unique'            => true
+            ],
+
+            'description' => [
+                'type'              => 'string',
+                'usage'             => 'text/plain',
+                'description'       => 'Short description of the Server.',
+            ],
+
+            'instances_ids' => [
+                'type'              => 'one2many',
+                'foreign_field'     => 'server_id',
+                'foreign_object'    => 'inventory\server\Instance',
+                'ondetach'          => 'delete',
+                'description'       => 'Instances running on the server.'
+            ],
+
+            'instances_count' => [
+                'type'              => 'computed',
+                'result_type'       => 'integer',
+                'function'          => 'calcInstancesCount'
+            ],
+
+        ];
+    }
+
+    protected static function calcInstancesCount($self): array {
+        $result = [];
+        $self->read(['instances_ids']);
+        foreach($self as $id => $server) {
+            $result[$id] = count($server['instances_ids']);
+        }
+
+        return $result;
+    }
+
+    public static function getActions(): array {
+        return [
+        ];
+    }
+
+
+}
