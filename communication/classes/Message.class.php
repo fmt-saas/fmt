0481b51584b7525f316de13789243a5df2d22336
Cédric Françoys <cedricfrancoys@gmail.com>
2025-10-21 18:41:44 +0200

    fixed licence notice

--- communication/classes/Message.class.php
+++ communication/classes/Message.class.php
@@ -1,8 +1,8 @@
 <?php
 /*
-    Developed by Yesbabylon – https://yesbabylon.com
-    (c) 2025–2026 Yesbabylon SA
-    Licensed under the GNU AGPL v3 License – https://www.gnu.org/licenses/agpl-3.0.html
+    Developed by Yesbabylon - https://yesbabylon.com
+    (c) 2025-2026 Yesbabylon SA
+    Licensed under the GNU AGPL v3 License - https://www.gnu.org/licenses/agpl-3.0.html
 */
 
 namespace communication;
