752a656f049403ac75b4db42a507d27c181b0ecd
Cédric Françoys <cedricfrancoys@gmail.com>
2025-11-05 14:27:19 +0100

    updated

--- fmt/actions/user/oauth/gmail.php
+++ fmt/actions/user/oauth/gmail.php
@@ -4,6 +4,8 @@
     (c) 2025-2026 Yesbabylon SA
     Licensed under the GNU AGPL v3 License - https://www.gnu.org/licenses/agpl-3.0.html
 */
+
+use equal\http\HttpRequest;
 use identity\User;
 
 // announce script and fetch parameters values
@@ -43,7 +45,8 @@ use identity\User;
             //'required'      => true
         ]
     ],
-    'access'      => [
+    'constants'     => ['BACKEND_URL'],
+    'access'        => [
         'visibility' => 'public'
     ],
     'response'      => [
@@ -62,7 +65,31 @@ use identity\User;
  */
 ['context' => $context, 'orm' => $om, 'auth' => $auth] = $providers;
 
+if($params['code']) {
+ // constant('BACKEND_URL')
+    $oauthRequest = new HttpRequest('https://oauth2.googleapis.com/token', ['Host' => 'graph.facebook.com:443']);
+    $response = $oauthRequest
+                ->header('Content-Type', 'application/x-www-form-urlencoded')
+                ->setBody([
+                    'grant_type' => 'authorization_code',
+                    'code' => $params['code'],
+                    'redirect_uri' => 'https://'.constant('BACKEND_URL').'/oauth/gmail',
+                    'client_id' => '24230475119-6fabc7k3lh9v9u3aa01im86d48bsudp0.apps.googleusercontent.com',
+                    'client_secret' => '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'
+                ])
+                ->send();
+
+    $data = $response->body();
+    $status = $response->getStatusCode();
+    ob_start();
+    var_dump($data);
+    $out = ob_get_clean();
+
+    file_put_contents(EQ_BASEDIR.'/log/test.gmail1.txt', $out);
+}
+
 $httpResponse = $context->httpResponse();
+
 
 ob_start();
 print_r($params);
