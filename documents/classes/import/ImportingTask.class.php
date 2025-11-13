84d9bec536bfcef8e88a88bfdc3271b1cd3c5326
Cédric Françoys <cedricfrancoys@gmail.com>
2025-11-13 09:31:11 +0100

    updated

---
+++ documents/classes/import/ImportingTask.class.php
@@ -0,0 +1,11 @@
+<?php
+// #todo - class to handle bulk import of documents (list of uploads, zip, ...)
+
+
+namespace documents\import;
+
+use equal\orm\Model;
+
+class ImportingTaskLog extends Model {
+
+}
