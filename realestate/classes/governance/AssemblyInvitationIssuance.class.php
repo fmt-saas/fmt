3a13268ac2a7cc80ead2b799dbc49b47aae71aa3
Cédric Françoys <cedricfrancoys@gmail.com>
2025-12-17 23:14:00 +0100

    updarted

---
+++ realestate/classes/governance/AssemblyInvitationIssuance.class.php
@@ -0,0 +1,17 @@
+<?php
+/*
+    Developed by Yesbabylon - https://yesbabylon.com
+    (c) 2025-2026 Yesbabylon SA
+    Licensed under the GNU AGPL v3 License - https://www.gnu.org/licenses/agpl-3.0.html
+*/
+namespace realestate\governance;
+
+class AssemblyInvitationIssuance extends \equal\orm\Model {
+
+    public static function getColumns() {
+
+        return [
+
+        ];
+    }
+}
