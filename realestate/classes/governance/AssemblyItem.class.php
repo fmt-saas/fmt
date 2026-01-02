411b63b1a453ead1e69986d66786c1a5e92a5f59
Cédric Françoys <cedricfrancoys@gmail.com>
2026-01-02 13:20:51 +0100

    updated

--- realestate/classes/governance/AssemblyItem.class.php
+++ realestate/classes/governance/AssemblyItem.class.php
@@ -879,7 +879,7 @@ class AssemblyItem extends AssemblyItemTemplate {
         $self->read(['assembly_id' => ['status']]);
         foreach($self as $id => $assembly) {
             if($assembly['assembly_id']['status'] !== 'pending') {
-                $allowed_fields = ['description_minutes', 'description_ballot', 'votes_ids', 'votes_count', 'vote_result'];
+                $allowed_fields = ['description_minutes', 'description_ballot', 'votes_ids', 'votes_count', 'vote_result', 'logs'];
                 if(count(array_diff(array_keys($values), $allowed_fields)) > 0) {
                     return ['assembly_id' => ['published_assembly_cannot_be_changed' => 'Once published, assembly items cannot be changed.']];
                 }
