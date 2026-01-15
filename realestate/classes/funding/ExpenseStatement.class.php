dce2154891ced65e2ebb2cbdfcd60995a57cccf2
Cédric Françoys <cedricfrancoys@gmail.com>
2026-01-15 12:43:50 +0100

    updated

--- realestate/classes/funding/ExpenseStatement.class.php
+++ realestate/classes/funding/ExpenseStatement.class.php
@@ -810,10 +810,6 @@ class ExpenseStatement extends \realestate\sale\accounting\invoice\SaleInvoice {
             ->read([
                 'accounting_entry_id',
                 'account_id', 'account_code', 'debit', 'credit',
-                // #memo - we need this to discard records from expense statements
-                'expense_statement_id',
-                // #memo - we need this to reject entry lines relating to fund requests global amounts
-                'fund_request_execution_id',
                 'sale_invoice_line_id',
                 // #memo - we need this to retrieve details for private expenses
                 'purchase_invoice_line_id',
@@ -832,7 +828,10 @@ class ExpenseStatement extends \realestate\sale\accounting\invoice\SaleInvoice {
                 'entry_date',
                 'status',
                 'journal_id',
-                'expense_statement_id'
+                // #memo - we need this to discard records from expense statements
+                'expense_statement_id',
+                // #memo - we need this to reject entry lines relating to fund requests global amounts
+                'fund_request_execution_id'
             ])
             ->get();
 
