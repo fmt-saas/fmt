c171eeb406f8afd54c88a8aa52f330de93bd063e
Cédric Françoys <cedricfrancoys@gmail.com>
2026-01-29 14:50:47 +0100

    updated

--- finance/classes/accounting/Account.class.php
+++ finance/classes/accounting/Account.class.php
@@ -456,19 +456,6 @@ class Account extends Model {
         return $result;
     }
 
-    public static function candelete($self) {
-        $self->read(['condo_id']);
-        foreach($self as $id => $account) {
-            if(!$account['condo_id']) {
-                continue;
-            }
-            $accounting_entries_ids = AccountingEntry::search([ ['condo_id', '=', $account['condo_id']], ['account_id', '=', $id] ])->ids();
-            if(count($accounting_entries_ids) > 0) {
-                return ['id' => ['non_removable' => 'Account with accounting entries cannot be removed.']];
-            }
-        }
-        return parent::candelete($self);
-    }
 
     public static function onchange($event, $values) {
         $result = [];
@@ -485,6 +472,32 @@ class Account extends Model {
         }
 
         return $result;
+    }
+
+    protected static function candelete($self) {
+        $self->read(['condo_id']);
+        foreach($self as $id => $account) {
+            if(!$account['condo_id']) {
+                continue;
+            }
+            $accounting_entries_ids = AccountingEntry::search([ ['condo_id', '=', $account['condo_id']], ['account_id', '=', $id] ])->ids();
+            if(count($accounting_entries_ids) > 0) {
+                return ['id' => ['non_removable' => 'Account with accounting entries cannot be removed.']];
+            }
+        }
+        return parent::candelete($self);
+    }
+
+    protected static function canupdate($self, $values) {
+        // cannot create account of class 1 (except for long-term debts)
+        if(isset($values['code'])) {
+            if(substr($values['code'], 0, 1) === '1') {
+                if(substr($values['code'], 0, 2) !== '17') {
+                    return ['code' => ['not_allowed' => 'Account of class 1 cannot be created manually.']];
+                }
+            }
+        }
+        return parent::canupdate($self);
     }
 
 }
