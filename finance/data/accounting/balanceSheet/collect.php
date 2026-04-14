e6a88ddc713b5661c543aae14d19c9b04b7bf3b9
Cédric Françoys <cedricfrancoys@gmail.com>
2026-04-14 13:34:34 +0200

    updated

--- finance/data/accounting/balanceSheet/collect.php
+++ finance/data/accounting/balanceSheet/collect.php
@@ -355,7 +355,7 @@ foreach($balances as $code => $balance) {
                 'account_id'   => $balance['account_id'],
                 'account_code' => $code,
                 'description'  => $balance['description'],
-                'balance'      => abs($raw),
+                'balance'      => $raw
             ];
         }
         // asset - increases by debit
