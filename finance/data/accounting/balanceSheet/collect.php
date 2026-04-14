3459d5877f28877b84449cc1033f5d646a06d70f
Cédric Françoys <cedricfrancoys@gmail.com>
2026-04-14 13:42:32 +0200

    updated

--- finance/data/accounting/balanceSheet/collect.php
+++ finance/data/accounting/balanceSheet/collect.php
@@ -376,7 +376,7 @@ foreach($balances as $code => $balance) {
                 'account_id'   => $balance['account_id'],
                 'account_code' => $code,
                 'description'  => $balance['description'],
-                'balance'      => $raw,
+                'balance'      => abs($raw),
             ];
         }
         else {
