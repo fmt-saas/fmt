3140861501056f8833b9dbb1dea0158e2d1401d9
Lucas <lucas@yesbabylon.com>
2026-04-16 12:02:55 +0200

    handle display value for old and new values of Update Request to ease approval

--- fmt/classes/sync/UpdateRequestLine.class.php
+++ fmt/classes/sync/UpdateRequestLine.class.php
@@ -7,6 +7,7 @@
 namespace fmt\sync;
 
 use equal\orm\Model;
+use fmt\setting\Setting;
 
 class UpdateRequestLine extends Model {
 
@@ -47,7 +48,7 @@ class UpdateRequestLine extends Model {
             'is_new' => [
                 'type'              => 'computed',
                 'result_type'       => 'boolean',
-                'relation'          => ['update_request_line_id' => 'is_new'],
+                'relation'          => ['update_request_id' => 'is_new'],
                 'store'             => true,
                 'description'       => 'JSON encoded new proposed value for the field.',
                 'default'           => false
@@ -58,11 +59,27 @@ class UpdateRequestLine extends Model {
                 'description'       => 'JSON encoded new proposed value for the field.'
             ],
 
+            'display_new_value' => [
+                'type'              => 'computed',
+                'result_type'       => 'string',
+                'description'       => "New value to display to user for it to be understandable easily.",
+                'store'             => false,
+                'function'          => 'calcDisplayNewValue'
+            ],
+
             'old_value' => [
                 'type'              => 'string',
                 'description'       => 'Old JSON value (currently stored).',
                 'visible'           => ['is_new', '=', false]
-            ]
+            ],
+
+            'display_old_value' => [
+                'type'              => 'computed',
+                'result_type'       => 'string',
+                'description'       => "Old value to display to user for it to be understandable easily.",
+                'store'             => false,
+                'function'          => 'calcDisplayOldValue'
+            ],
 
         ];
     }
@@ -85,4 +102,81 @@ class UpdateRequestLine extends Model {
         return $result;
     }
 
+    public static function calcDisplayNewValue($self, $orm): array {
+        $date_format = null;
+        $time_format = null;
+
+        $result = [];
+        $self->read(['object_class', 'object_field', 'new_value']);
+        foreach($self as $id => $line) {
+            $model = $orm->getModel($line['object_class']);
+            $schema = $model->getSchema();
+
+            $field_def = $schema[$line['object_field']];
+            $type = $field_def['result_type'] ?? ($field_def['type'] ?? '');
+
+            $display_value = $line['new_value'];
+            if(is_null($line['new_value'])) {
+                $display_value = 'NULL';
+            }
+            elseif($type === 'date') {
+                if(is_null($date_format)) {
+                    $date_format = Setting::get_value('core', 'locale', 'date_format', 'm/d/Y');
+                }
+                $display_value = date($date_format, strtotime($line['new_value']));
+            }
+            elseif($type === 'datetime') {
+                if(is_null($date_format)) {
+                    $date_format = Setting::get_value('core', 'locale', 'date_format', 'm/d/Y');
+                }
+                if(is_null($time_format)) {
+                    $time_format = Setting::get_value('core', 'locale', 'time_format', 'H:i:s');
+                }
+                $display_value = date($date_format.' '.$time_format, strtotime($line['new_value']));
+            }
+
+            $result[$id] = $display_value;
+        }
+
+        return $result;
+    }
+
+    public static function calcDisplayOldValue($self, $orm): array {
+        $date_format = null;
+        $time_format = null;
+
+        $result = [];
+        $self->read(['object_class', 'object_field', 'is_new', 'old_value']);
+        foreach($self as $id => $line) {
+            $model = $orm->getModel($line['object_class']);
+            $schema = $model->getSchema();
+
+            $field_def = $schema[$line['object_field']];
+            $type = $field_def['result_type'] ?? ($field_def['type'] ?? '');
+
+            $display_value = $line['old_value'];
+            if(is_null($line['old_value']) && !$line['is_new']) {
+                $display_value = 'NULL';
+            }
+            elseif(!is_null($line['old_value']) && $type === 'date') {
+                if(is_null($date_format)) {
+                    $date_format = Setting::get_value('core', 'locale', 'date_format', 'm/d/Y');
+                }
+                $display_value = date($date_format, strtotime($line['old_value']));
+            }
+            elseif(!is_null($line['old_value']) && $type === 'datetime') {
+                if(is_null($date_format)) {
+                    $date_format = Setting::get_value('core', 'locale', 'date_format', 'm/d/Y');
+                }
+                if(is_null($time_format)) {
+                    $time_format = Setting::get_value('core', 'locale', 'time_format', 'H:i:s');
+                }
+                $display_value = date($date_format.' '.$time_format, strtotime($line['old_value']));
+            }
+
+            $result[$id] = $display_value;
+        }
+
+        return $result;
+    }
 }
