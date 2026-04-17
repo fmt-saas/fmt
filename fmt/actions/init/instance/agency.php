f53554573b0b96e3b8da3684927b9333e2908fdc
Lucas <lucas@yesbabylon.com>
2026-04-17 09:58:05 +0200

    enhance handling of the init of an agency instance

--- fmt/actions/init/instance/agency.php
+++ fmt/actions/init/instance/agency.php
@@ -14,10 +14,15 @@ use infra\server\Instance;
             'type'          => 'string',
             'description'   => "The UUID of the agency instance, it is generated on global instance."
         ],
+        'api_internal_token' => [
+            'type'          => 'string',
+            'description'   => "The token to access global instance's API."
+        ],
         'sync' => [
             'type'          => 'boolean',
             'description'   => "Must the new agency instance be synchronized with global instance ?",
-            'default'       => false
+            'help'          => "Default true because it'll mostly be used with synchronisation.",
+            'default'       => true
         ]
     ],
     'access' => [
@@ -41,15 +46,32 @@ if($params['sync']) {
         throw new Exception('uuid_must_be_provided_to_sync_with_global', EQ_ERROR_INVALID_PARAM);
     }
 
-    if(empty(constant('FMT_API_INTERNAL_TOKEN'))) {
-        throw new Exception('fmt_api_internal_token_config_needed_to_sync_with_global', EQ_ERROR_NOT_ALLOWED);
+    if(empty($params['api_internal_token']) && empty(constant('FMT_API_INTERNAL_TOKEN'))) {
+        throw new Exception('api_internal_token_needed_to_sync_with_global', EQ_ERROR_NOT_ALLOWED);
+    }
+
+    // add given api internal token in config.json
+    if(!empty($params['api_internal_token'])) {
+        $config = [];
+        if(file_exists(EQ_BASEDIR . '/config/config.json')) {
+            $json = file_get_contents(EQ_BASEDIR . '/config/config.json');
+            $data = json_decode($json, true);
+            if($data) {
+                $config = $data;
+            }
+        }
+
+        $config['FMT_API_INTERNAL_TOKEN'] = $params['api_internal_token'];
+
+        file_put_contents(EQ_BASEDIR . '/config/config.json', json_encode($config, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+
+        config\define('FMT_API_INTERNAL_TOKEN', $params['api_internal_token']);
     }
 }
 
 if(constant('FMT_INSTANCE_TYPE') !== 'agency') {
     throw new Exception('invalid_instance_type', EQ_ERROR_NOT_ALLOWED);
 }
-// test that api conf is set
 
 $map_init_packages = [
     'core'      => false,
@@ -96,7 +118,7 @@ if(!empty($params['instance_uuid'])) {
     Instance::id(1)->update(['uuid' => $params['instance_uuid']]);
 
     if($params['sync']) {
-        eQual::run('do', 'fmt_sync_SyncPolicy_pull-from-global');
+        eQual::run('do', 'fmt_sync_SyncPolicy_pull-from-global', ['reset' => true]);
         eQual::run('do', 'fmt_sync_pull-from-global', ['accept' => true]);
     }
 }
