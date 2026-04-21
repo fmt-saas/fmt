ed4eb9564ab6b58acecc07b304a81fbf74b51270
Cédric Françoys <cedricfrancoys@gmail.com>
2026-04-21 15:09:59 +0200

    added settings

--- fmt/init/data/scripts/10-create_settings.php
+++ fmt/init/data/scripts/10-create_settings.php
@@ -20,3 +20,23 @@ Setting::assert_sequence('finance', 'accounting', 'accounting_entry.sequence', 1
 
 Setting::assert_value('identity', 'organization', 'identity_type_default', 3);
 Setting::assert_value('identity', 'organization', 'identity_lang_default', 2);
+
+// Affichage du détail par lot (annexe)
+Setting::assert_value('realestate', 'features', 'expense_statement.show_lots_details', 0);
+// Présentation regroupée par lot principal
+Setting::assert_value('realestate', 'features', 'expense_statement.enable_lots_grouping', 1);
+// Affichage du détail TVA
+Setting::assert_value('realestate', 'features', 'expense_statement.show_vat_detail', 0);
+// Distinction propriétaire / locataire
+Setting::assert_value('realestate', 'features', 'expense_statement.show_owner_tenant_split', 1);
+// Addition de la part occupant dans la part proprio
+/*
+    colonne LOC toujours à titre indicatif (montant qui peut être demandé au LOC)
+    - enabled: colonne PROP = LOT
+    - disabled: colonne PROPLOT - LOC
+*/
+Setting::assert_value('realestate', 'features', 'expense_statement.enable_tenant_rollup', 0);
+// Regroupement par compte collecteur (contrôle)
+Setting::assert_value('realestate', 'features', 'expense_statement.enable_accounts_grouping', 0);
+// Forcer nombre de pages pair
+Setting::assert_value('realestate', 'features', 'expense_statement.enable_force_even_pages', 1);
