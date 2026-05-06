5de40bdb5e5693bc5b883db1a59fe280ce03f072
Lucas <lucas@yesbabylon.com>
2026-05-06 16:42:11 +0200

    make creation of token from interface possible

--- infra/actions/server/Instance/token.php
+++ infra/actions/server/Instance/token.php
@@ -5,27 +5,33 @@
     Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
 */
 
-use core\security\AccessToken;
 use infra\server\Instance;
 
 [$params, $providers] = eQual::announce([
-    'description'	=>	"Generates an API token for the user linked to the given instance.",
-    'help'	        =>	"This controller allows to provide secret API keys to `agency` instances if on `global`, to `global` instance if on `agency`.",
-    'params' 		=>	[
+    'description'	=> "Generates an API token for the user linked to the given instance.",
+    'help'	        => "This controller allows to provide secret API keys to `agency` instances if on `global`, to `global` instance if on `agency`.",
+    'params' 		=> [
         'id' => [
             'type'              => 'many2one',
             'description'       => "Instance id for which a token is requested.",
             'foreign_object'    => 'infra\server\Instance',
             'required'          => true
+        ],
+
+        'save_in_description' => [
+            'type'              => 'boolean',
+            'description'       => "Save the access token in the instance's description for creation from interface. This allows the copy/paste of the access token and then its removal from the description.",
+            'default'           => false
         ]
     ],
     'access'      => [
-        'visibility' => 'private'
+        'visibility'    => 'protected',
+        'groups'        => ['admins']
     ],
     'response'      => [
-        'content-type'      => 'application/json',
-        'charset'           => 'utf-8',
-        'accept-origin'     => '*'
+        'content-type'  => 'application/json',
+        'charset'       => 'utf-8',
+        'accept-origin' => '*'
     ],
     'constants'     => ['FMT_INSTANCE_TYPE'],
     'providers'     => ['context', 'auth']
@@ -55,8 +61,18 @@ try {
     $access_token = $auth->createAccessToken($instance['user_id']);
 }
 catch(Exception $e) {
-    AccessToken::id($token['id'])->delete(true);
     throw new Exception("Error while generating token: ".$e->getMessage(), EQ_ERROR_UNKNOWN);
+}
+
+if(isset($access_token) && $params['save_in_description']) {
+    $instance = Instance::id($params['id'])
+        ->read(['description'])
+        ->first();
+
+    $description = !empty($instance['description']) ? $instance['description'].PHP_EOL.PHP_EOL : '';
+    $description .= 'Access token (TO REMOVE AFTER COPY): '.$access_token;
+
+    Instance::id($params['id'])->update(['description' => $description]);
 }
 
 $context
