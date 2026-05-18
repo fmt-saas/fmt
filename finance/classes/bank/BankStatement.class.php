86aea100bc54dc571fca2b219b9463fd1e222139
Cédric Françoys <cedricfrancoys@gmail.com>
2026-05-18 13:20:21 +0200

    updated

--- finance/classes/bank/BankStatement.class.php
+++ finance/classes/bank/BankStatement.class.php
@@ -364,7 +364,7 @@ class BankStatement extends Model {
             }
 
             try {
-                $bankStatement['statement_lines_ids']->assert('can_post');
+                $bankStatement['statement_lines_ids']->assert('can_generate_accounting_entry');
             }
             catch(\Exception $e) {
                 $result[$id] = [
