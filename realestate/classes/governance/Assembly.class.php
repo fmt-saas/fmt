5e8bc70c97d6763a36c050f74d9941314e06c5c4
Cédric Françoys <cedricfrancoys@gmail.com>
2026-06-15 15:45:58 +0200

    updated

--- realestate/classes/governance/Assembly.class.php
+++ realestate/classes/governance/Assembly.class.php
@@ -99,7 +99,7 @@ class Assembly extends \equal\orm\Model {
                     ['condo_id', '=', 'object.condo_id'],
                     ['condo_id', '<>', null],
                     ['assembly_item_id', '<>', null],
-                    ['assembly_id', '=', 'object.assembly_id']
+                    ['assembly_id', '=', 'object.id']
                 ]
             ],
 
