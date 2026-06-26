167bd68d7396cab12d7d480bd301de1757527bdc
Cédric Françoys <cedricfrancoys@gmail.com>
2026-06-26 16:37:58 +0200

    updated

--- documents/data/Document/add-overlay.php
+++ documents/data/Document/add-overlay.php
@@ -78,6 +78,25 @@ use equal\text\TextTransformer;
             'max'               => 50
         ],
 
+        'page_size' => [
+            'type'              => 'string',
+            'description'       => "The expected page size of the PDF document.",
+            'help'              => "Used to normalize the page CropBox and resize calculations. Values are ISO page sizes.",
+            'selection'         => ['A1', 'A2', 'A3', 'A4', 'A5'],
+            'default'           => 'A4'
+        ],
+
+        'page_orientation' => [
+            'type'              => 'string',
+            'description'       => "The expected page orientation of the PDF document.",
+            'help'              => "Used to swap page width and height when landscape orientation is selected.",
+            'selection'         => [
+                'portrait',
+                'landscape'
+            ],
+            'default'           => 'portrait'
+        ],
+
         'overlay_text' => [
             'type'              => 'string',
             'description'       => "The text value to use as overlay.",
@@ -97,22 +116,45 @@ use equal\text\TextTransformer;
  */
 ['context' => $context] = $providers;
 
+$formats = [
+    'A1' => ['width' => 1684, 'height' => 2384],
+    'A2' => ['width' => 1191, 'height' => 1684],
+    'A3' => ['width' => 842,  'height' => 1191],
+    'A4' => ['width' => 595,  'height' => 842],
+    'A5' => ['width' => 420,  'height' => 595]
+];
+
+$format = $formats[$params['page_size']];
+
+$width = $format['width'];
+$height = $format['height'];
+
+if($params['page_orientation'] === 'landscape') {
+    [$width, $height] = [$height, $width];
+}
+
 /**
  * Methods
  */
 
-$resize = function($pdf_file, $scale) {
+$resize = function($pdf_content, $scale, $width, $height) use($params) {
     $output_file = tempnam(sys_get_temp_dir(), 'resized_');
+    $pdf_file = tempnam(sys_get_temp_dir(), 'pdf_');
 
-    $total_empty_width  = 595 * (1 - $scale);
-    $total_empty_height = 842 * (1 - $scale);
+    file_put_contents($pdf_file, $pdf_content);
+
+    $total_empty_width  = $width * (1 - $scale);
+    $total_empty_height = $height * (1 - $scale);
 
     $offset_x = $total_empty_width / 2;
     $offset_y = $total_empty_height / 2;
 
+    $paper_size = strtolower($params['page_size']);
+
     $gs_cmd = sprintf(
-        'gs -o %s -dSAFER -sDEVICE=pdfwrite -sPAPERSIZE=a4 -dFIXEDMEDIA -dPDFFitPage -c "<</BeginPage {%s %s translate %s %s scale}>> setpagedevice" -f %s',
+        'gs -o %s -dSAFER -sDEVICE=pdfwrite -sPAPERSIZE=%s -dFIXEDMEDIA -dPDFFitPage -c "<</BeginPage {%s %s translate %s %s scale}>> setpagedevice" -f %s',
         escapeshellarg($output_file),
+        escapeshellarg($paper_size),
         $offset_x,
         $offset_y,
         $scale,
@@ -126,11 +168,19 @@ $resize = function($pdf_file, $scale) {
         throw new Exception("resized_failed", EQ_ERROR_UNKNOWN);
     }
 
-    return $output_file;
+    $output = file_get_contents($output_file);
+
+    @unlink($output_file);
+    @unlink($pdf_file);
+
+    return $output;
 };
 
-$addOverlay = function($pdf_file, $overlay_text, $font_size, $pos_x, $pos_y) use($params) {
+$addOverlay = function($pdf_content, $overlay_text, $font_size, $pos_x, $pos_y) use($params) {
     $output_file = tempnam(sys_get_temp_dir(), 'overlay_');
+    $pdf_file = tempnam(sys_get_temp_dir(), 'pdf_');
+
+    file_put_contents($pdf_file, $pdf_content);
 
     // handle special characters for PostScript: convert utf8 -> ascii
     $overlay_text = TextTransformer::toAscii($overlay_text);
@@ -162,19 +212,19 @@ $addOverlay = function($pdf_file, $overlay_text, $font_size, $pos_x, $pos_y) use
     ][$params['font']] ?? 'Helvetica';
 
     $ps_content = <<<PS
-%!PS
-<<
-  /BeginPage {
-    gsave
-    /$font findfont $font_size scalefont setfont
-    0 setgray
+    %!PS
+    <<
+    /BeginPage {
+        gsave
+        /$font findfont $font_size scalefont setfont
+        0 setgray
 
-    $pos_x $pos_y moveto
-    ($overlay_text) show
-    grestore
-  }
->> setpagedevice
-PS;
+        $pos_x $pos_y moveto
+        ($overlay_text) show
+        grestore
+    }
+    >> setpagedevice
+    PS;
 
     $ps_file = tempnam(sys_get_temp_dir(), 'overlay_ps_');
     file_put_contents($ps_file, $ps_content);
@@ -192,9 +242,13 @@ PS;
         throw new Exception("overlay_creation_failed", EQ_ERROR_UNKNOWN);
     }
 
+    $output = file_get_contents($output_file);
+
+    @unlink($output_file);
+    @unlink($pdf_file);
     @unlink($ps_file);
 
-    return $output_file;
+    return $output;
 };
 
 /**
@@ -218,21 +272,22 @@ $filename = $document['name'];
 
 $scale = round($params['resize'], 2);
 
-$temp_file = tempnam(sys_get_temp_dir(), 'pdf_');
-file_put_contents($temp_file, $document['data']);
+$pdf_content = $document['data'];
 
 if($scale < 1) {
-    $temp_resized_file = $resize($temp_file, $scale);
-    @unlink($temp_file);
-
-    $temp_file = $temp_resized_file;
+    $pdf_content = $resize($pdf_content, $scale, $width, $height);
 }
 
-$tmp_file_with_overlay = $addOverlay($temp_file, $params['overlay_text'], $params['font_size'], $params['pos_x'], $params['pos_y']);
-@unlink($temp_file);
+$pdf_content = $addOverlay($pdf_content, $params['overlay_text'], $params['font_size'], $params['pos_x'], $params['pos_y']);
 
-$output = file_get_contents($tmp_file_with_overlay);
-@unlink($tmp_file_with_overlay);
+// Normalize existing CropBox entries to the expected full page size to avoid
+// cropped rendering in downstream PDF viewers/converters. This output is a
+// technical copy; the original document remains unchanged.
+$output = preg_replace(
+    '#/CropBox\s*\[\s*[-+]?\d*\.?\d+\s+[-+]?\d*\.?\d+\s+[-+]?\d*\.?\d+\s+[-+]?\d*\.?\d+\s*\]#s',
+    "/CropBox [0 0 {$width} {$height}]",
+    $pdf_content
+);
 
 $context->httpResponse()
         ->header('Content-Disposition', $params['disposition'] . '; filename="' . $filename . '"')
