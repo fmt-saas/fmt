0770a5cb0e8894762e42de51a50b6e38888c46c6
Cédric Françoys <cedricfrancoys@gmail.com>
2026-07-08 16:20:17 +0200

    updated

--- realestate/actions/sale/pay/Funding/bulk-send.php
+++ realestate/actions/sale/pay/Funding/bulk-send.php
@@ -82,11 +82,6 @@ foreach($fundings as $funding_id => $funding) {
         'id' => $funding_id
     ]);
 
-    Funding::id($funding_id)
-        ->update([
-            'is_sent'      => true
-        ]);
-
 }
 
 $context->httpResponse()
