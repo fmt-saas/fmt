2bfa63dd082a90aa32fd6138daf27a4e47f843d7
Cédric Françoys <cedricfrancoys@gmail.com>
2026-07-08 14:54:00 +0200

    updated

--- realestate/classes/purchase/accounting/invoice/PurchaseInvoice.class.php
+++ realestate/classes/purchase/accounting/invoice/PurchaseInvoice.class.php
@@ -1231,6 +1231,8 @@ class PurchaseInvoice extends \purchase\accounting\invoice\PurchaseInvoice {
                 }
             }
 
+            Funding::ids($invoice['fundings_ids']->ids())->update(['name' => null]);
+
             // reset computed relation fields
             self::id($id)->update([
                     'document_process_status' => null,
