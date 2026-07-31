ee41d7af3bf43e164b36508607fdcf2d53c5158d
Cédric Françoys <cedricfrancoys@gmail.com>
2026-07-31 14:35:10 +0200

    updated

--- communication/classes/broadcast/Broadcast.class.php
+++ communication/classes/broadcast/Broadcast.class.php
@@ -27,7 +27,6 @@ class Broadcast extends Model {
                 'foreign_object'    => 'realestate\property\Condominium',
                 'onupdate'          => 'onupdateCondoId',
                 'required'          => true,
-                'readonly'          => true,
                 'help'              => 'This field is meant to be set only once at creation'
             ],
 
