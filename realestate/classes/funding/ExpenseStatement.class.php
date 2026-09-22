34ff2924617248089289a9c10216e8991cde94e3
Cédric Françoys <cedricfrancoys@gmail.com>
2026-09-22 10:01:34 +0200

    added support for unlink Funding & FundingAllocation in doUnlock

--- realestate/classes/funding/ExpenseStatement.class.php
+++ realestate/classes/funding/ExpenseStatement.class.php
@@ -391,7 +391,9 @@ class ExpenseStatement extends \realestate\sale\accounting\invoice\SaleInvoice {
 
         foreach($self as $id => $expenseStatement) {
             // retrieve accounting entry and cancel it
-            AccountingEntry::id($expenseStatement['accounting_entry_id'])->do('cancel');
+            if($expenseStatement['accounting_entry_id']) {
+                AccountingEntry::id($expenseStatement['accounting_entry_id'])->do('cancel');
+            }
 
             // un-clear accounting entry lines impacted by the statement
             AccountingEntryLine::search(['clearing_expense_statement_id', '=', $id])
@@ -401,15 +403,88 @@ class ExpenseStatement extends \realestate\sale\accounting\invoice\SaleInvoice {
                 ]);
 
             // remove related fundings (move payments to BankStatementLine Funding if any)
-            Funding::search([
+            $funding_ids = Funding::search([
                     ['condo_id', '=', $expenseStatement['condo_id']],
                     ['funding_type', '=', 'expense_statement'],
                     ['expense_statement_id', '=', $id]
                 ])
-                ->do('remove');
+                ->ids();
+
+            $map_funding_ids = [];
+            $map_funding_allocation_ids = [];
+
+            foreach($funding_ids as $funding_id) {
+                $map_funding_ids[$funding_id] = true;
+            }
+
+            if(count($funding_ids) > 0) {
+                $fundingAllocations = FundingAllocation::search([
+                        ['funding_id', 'in', $funding_ids],
+                        ['payment_origin', '=', 'funding_allocation']
+                    ])
+                    ->read(['linked_payment_id']);
+
+                $direct_funding_allocation_ids = [];
+                foreach($fundingAllocations as $funding_allocation_id => $fundingAllocation) {
+                    $direct_funding_allocation_ids[] = $funding_allocation_id;
+                    $map_funding_allocation_ids[$funding_allocation_id] = true;
+
+                    if($fundingAllocation['linked_payment_id']) {
+                        $map_funding_allocation_ids[$fundingAllocation['linked_payment_id']] = true;
+                    }
+                }
+
+                if(count($direct_funding_allocation_ids) > 0) {
+                    $linked_funding_allocation_ids = FundingAllocation::search([
+                            ['linked_payment_id', 'in', $direct_funding_allocation_ids],
+                            ['payment_origin', '=', 'funding_allocation']
+                        ])
+                        ->ids();
+
+                    foreach($linked_funding_allocation_ids as $funding_allocation_id) {
+                        $map_funding_allocation_ids[$funding_allocation_id] = true;
+                    }
+                }
+            }
+
+            if(count($map_funding_allocation_ids) > 0) {
+                $fundingAllocations = FundingAllocation::ids(array_keys($map_funding_allocation_ids))
+                    ->read(['funding_id', 'payment_origin']);
+
+                $map_funding_allocation_ids = [];
+                foreach($fundingAllocations as $funding_allocation_id => $fundingAllocation) {
+                    if($fundingAllocation['payment_origin'] !== 'funding_allocation') {
+                        continue;
+                    }
+
+                    $map_funding_allocation_ids[$funding_allocation_id] = true;
+                    if($fundingAllocation['funding_id']) {
+                        $map_funding_ids[$fundingAllocation['funding_id']] = true;
+                    }
+                }
+
+                if(count($map_funding_allocation_ids) > 0) {
+                    FundingAllocation::ids(array_keys($map_funding_allocation_ids))->delete(true);
+                }
+            }
+
+            if(count($funding_ids) > 0) {
+                Funding::ids($funding_ids)->do('remove');
+
+                foreach($funding_ids as $funding_id) {
+                    unset($map_funding_ids[$funding_id]);
+                }
+            }
+
+            if(count($map_funding_ids) > 0) {
+                Funding::ids(array_keys($map_funding_ids))->do('refresh_status');
+            }
         }
 
-        $self->update(['status' => 'proforma']);
+        $self->update([
+                'status'              => 'proforma',
+                'accounting_entry_id' => null
+            ]);
     }
 
     private static function normalizeMoneyAmount($amount): float {
